ebd4761635c628b58fc43c4c74c85f0a1ce0a71f
baonguyen2310 <lanchinhaicon@gmail.com>
2023-08-26 11:49:19 +0700

    meta data og image post

--- app/(website)/post/[slug]/page.js
+++ app/(website)/post/[slug]/page.js
@@ -1,14 +1,33 @@
 import PostPage from "./default";
 
 import { getAllPostsSlugs, getPostBySlug } from "@/lib/sanity/client";
+import { urlForImage } from "@/lib/sanity/image";
 
 export async function generateStaticParams() {
   return await getAllPostsSlugs();
 }
 
+
 export async function generateMetadata({ params }) {
   const post = await getPostBySlug(params.slug);
-  return { title: post.title };
+  return {
+    title: post?.title,
+    openGraph: {
+      images: [
+        {
+          url:
+            urlForImage(post?.mainImage)?.src ||
+            "/img/opengraph.jpg",
+          width: 800,
+          height: 600
+        }
+      ]
+    },
+    twitter: {
+      title: post?.title || "DFO Tay Ninh",
+      card: "summary_large_image"
+    },
+  };
 }
 
 export default async function PostDefault({ params }) {
